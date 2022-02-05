97293dfcaca2eda4ee1872ec9f360f255a0e4387
Dimas Saputra <dimas.maulanads@gmail.com>
2022-02-05 10:31:49 +0700

    personal information implemented

--- src/components/Pages/HomePage/index.js
+++ src/components/Pages/HomePage/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Navigation from '../../Pures/Navigation';
 import { navigation } from '../../../content';
+import PersonalInformation from '../../Pures/PersonalInformation';
+import './style.scss';
 
 function HomePage() {
   const { title } = navigation;
@@ -8,7 +10,11 @@ function HomePage() {
   return (
     <>
       <Navigation title={title} />
-      <main />
+      <main>
+        <div className="main__container">
+          <PersonalInformation />
+        </div>
+      </main>
     </>
   );
 }
