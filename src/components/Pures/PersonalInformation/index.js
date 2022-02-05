54f32ec2e9d79de7ec0307df55b80c352ecda621
Dimas Saputra <dimas.maulanads@gmail.com>
2022-02-06 06:36:48 +0700

    fix mini info

--- src/components/Pures/PersonalInformation/index.js
+++ src/components/Pures/PersonalInformation/index.js
@@ -27,43 +27,49 @@ function PersonalInformation() {
       />
       <h2 className="personal-information__name">{name}</h2>
       <p className="personal-information__mini_info">
-        <span>{`@${mention}`}</span>
-        <span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z" />
-            <circle cx="12" cy="10" r="3" />
-          </svg>
-          {location}
-        </span>
-        <span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <polyline points="16 18 22 12 16 6" />
-            <polyline points="8 6 2 12 8 18" />
-          </svg>
-          {`${role} at `}
-          {' '}
-          <strong>{at}</strong>
-        </span>
+        <div>
+          <span>{`@${mention}`}</span>
+        </div>
+        <div>
+          <span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z" />
+              <circle cx="12" cy="10" r="3" />
+            </svg>
+            {location}
+          </span>
+        </div>
+        <div>
+          <span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <polyline points="16 18 22 12 16 6" />
+              <polyline points="8 6 2 12 8 18" />
+            </svg>
+            {`${role} at `}
+            {' '}
+            <strong>{at}</strong>
+          </span>
+        </div>
       </p>
       <p className="personal-information__description">{description}</p>
       <Tags />
