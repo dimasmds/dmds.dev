8cb7a28d42907da8338801cc1f09e74ebb5d68a3
Dimas Saputra <dimas.maulanads@gmail.com>
2022-02-05 08:48:50 +0700

    initial header

--- src/components/Pures/Navigation/index.js
+++ src/components/Pures/Navigation/index.js
@@ -32,32 +32,34 @@ class Navigation extends Component {
 
     return (
       <header className="header">
-        <button
-          type="button"
-          className="header__drawer_button"
-          onClick={this.onDrawerButtonClick}
-        >
-          ☰
-        </button>
-        <h1 className="header__title">{title}</h1>
-        <nav className={`header__navigation_drawer ${isDrawerOpen ? 'open' : ''}`}>
+        <div className="header__container">
           <button
             type="button"
-            onClick={this.onCloseButtonClick}
-            className="header__navigation_drawer_close"
+            className="header__drawer_button"
+            onClick={this.onDrawerButtonClick}
           >
-            ×
+            ☰
           </button>
-          <ul>
-            {
-              navigation.menus.map(({ name, url }) => (
-                <li key={name}>
-                  <a href={url}>{name}</a>
-                </li>
-              ))
-            }
-          </ul>
-        </nav>
+          <h1 className="header__title">{title}</h1>
+          <nav className={`header__navigation_drawer ${isDrawerOpen ? 'open' : ''}`}>
+            <button
+              type="button"
+              onClick={this.onCloseButtonClick}
+              className="header__navigation_drawer_close"
+            >
+              ×
+            </button>
+            <ul>
+              {
+                navigation.menus.map(({ name, url }) => (
+                  <li key={name}>
+                    <a href={url}>{name}</a>
+                  </li>
+                ))
+              }
+            </ul>
+          </nav>
+        </div>
       </header>
     );
   }
