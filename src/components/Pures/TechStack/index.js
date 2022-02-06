998b603708212b70149cfaa91af620e9ee518589
Dimas Saputra <dimas.maulanads@gmail.com>
2022-02-06 17:45:00 +0700

    fix semantic heading

--- src/components/Pures/TechStack/index.js
+++ src/components/Pures/TechStack/index.js
@@ -6,13 +6,13 @@ import './style.scss';
 function TechStack() {
   return (
     <div className="tech-stack">
-      <h2 className="tech-stack__title">Tech Stack</h2>
+      <h3 className="tech-stack__title">Tech Stack</h3>
       <div className="tech-stack__stack__list">
         {
         Object.keys(techStack)
           .map((key) => (
             <div key={key} className="tech-stack__stack">
-              <h3>{key}</h3>
+              <h4>{key}</h4>
               {techStack[key].map(({ name, icon }) => (
                 <div key={name} className="tech-stack__stack__item">
                   <img src={icon} alt={name} />
