e6e0c3cc9f06612335dc06442cec81c65b01536f
Dimas Saputra <dimas@dicoding.com>
2022-02-13 22:24:03 +0700

    about page implemented

---
+++ public/contents/about/about.md
@@ -0,0 +1,11 @@
+### Hi! 👋 I'm Dimas.
+
+An independent Software Engineer who loves to create and share projects about web and mobile technology! I've enjoyed JavaScript and make it the main language in creating a project.
+
+I'm currently active in contributing as Curriculum Developer at [@dicodingacademy](https://dicoding.com/) to build courses in Front-End Apps (Web, Android) and Back-End Service.
+
+- 🔭 I’m currently working on [@dicodingacademy](https://dicoding.com/)
+- 🌱 I’m currently learning about cloud technology for back-end apps
+- 💬 Feel free to ask me about web development or JavaScript
+- 📫 How to reach me: dimas@dicoding.com
+- ⚡ Fun fact: Father of a daughter
--- src/components/Pages/AboutPage/index.js
+++ src/components/Pages/AboutPage/index.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import ReactMarkdown from 'react-markdown';
 import Navigation from '../../Pures/Navigation';
 import { navigation } from '../../../content';
+import Footer from '../../Pures/Footer';
+
+import './style.scss';
 
 function AboutPage() {
   const { title } = navigation;
+  const [aboutText, setAboutText] = React.useState('');
+
+  useEffect(async () => {
+    const response = await fetch('/contents/about/about.md');
+    const responseText = await response.text();
+    setAboutText(responseText);
+  });
+
   return (
-    <Navigation title={title} activeMenu="About" />
+    <>
+      <Navigation title={title} activeMenu="About" />
+      <div className="page-container">
+        <main className="about">
+          <h2 className="about__title">About me</h2>
+          <div className="about__content">
+            <ReactMarkdown>
+              {aboutText}
+            </ReactMarkdown>
+          </div>
+        </main>
+        <Footer />
+      </div>
+    </>
   );
 }
 
