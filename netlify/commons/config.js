d8105cbd130a31dd84996066ee63294cfcdb4dea
Dimas Saputra <dimas.maulanads@gmail.com>
2022-02-19 23:35:37 +0700

    remove unneeded property

--- netlify/commons/config.js
+++ netlify/commons/config.js
@@ -4,7 +4,6 @@ dotenv.config();
 
 const config = {
   spotify: {
-    token: process.env.SPOTIFY_TOKEN,
     clientId: process.env.SPOTIFY_CLIENT_ID,
     clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
     refreshToken: process.env.SPOTIFY_REFRESH_TOKEN,
