e5663292776992b81b07af7c37c2f1fc8a334614
Dimas Saputra <dimas.maulanads@gmail.com>
2022-02-19 23:34:19 +0700

    configuring tokenize spotify

--- netlify/commons/config.js
+++ netlify/commons/config.js
@@ -5,6 +5,9 @@ dotenv.config();
 const config = {
   spotify: {
     token: process.env.SPOTIFY_TOKEN,
+    clientId: process.env.SPOTIFY_CLIENT_ID,
+    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+    refreshToken: process.env.SPOTIFY_REFRESH_TOKEN,
   },
 };
 
--- netlify/commons/network.js
+++ netlify/commons/network.js
@@ -1,10 +1,37 @@
 const fetch = require('node-fetch');
+const config = require('./config');
 
-const fetchWithJwtAuth = async (url, token, options = {}) => {
+const getAccessTokenSpotify = async (authOptions) => {
+  const {
+    clientId,
+    clientSecret,
+    refreshToken,
+  } = authOptions;
+
+  const response = await fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: `grant_type=refresh_token&refresh_token=${refreshToken}&client_id=${clientId}&client_secret=${clientSecret}`,
+  });
+
+  const json = await response.json();
+  return json.access_token;
+};
+
+const fetchSpotifyAuth = async (url, options = {}) => {
+  const token = await getAccessTokenSpotify({
+    clientId: config.spotify.clientId,
+    clientSecret: config.spotify.clientSecret,
+    refreshToken: config.spotify.refreshToken,
+  });
+
   const headers = {
     Authorization: `Bearer ${token}`,
     'Content-Type': 'application/json',
   };
+
   const opts = {
     ...options,
     headers,
@@ -13,4 +40,4 @@ const fetchWithJwtAuth = async (url, token, options = {}) => {
   return fetch(url, opts);
 };
 
-module.exports = { fetchWithJwtAuth };
+module.exports = { fetchWithJwtAuth: fetchSpotifyAuth };
