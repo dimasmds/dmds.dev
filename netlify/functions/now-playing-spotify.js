aa52be84a003f336443b12e0f18262a5b1fac281
Dimas Saputra <dimas.maulanads@gmail.com>
2022-02-19 21:53:59 +0700

    add error handling

--- netlify/functions/now-playing-spotify.js
+++ netlify/functions/now-playing-spotify.js
@@ -3,12 +3,33 @@ const { fetchWithJwtAuth } = require('../commons/network');
 
 exports.handler = async () => {
   const response = await fetchWithJwtAuth('https://api.spotify.com/v1/me/player/currently-playing', config.spotify.token);
+
+  if (!response.status.toString().startsWith('2')) {
+    return {
+      statusCode: response.status,
+      body: JSON.stringify({
+        message: 'Error fetching current track',
+        data: null,
+      }),
+    };
+  }
+
+  if (response.status === 204) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'No song is currently playing',
+        data: null,
+      }),
+    };
+  }
+
   const data = await response.json();
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Hello from Now Playing Spotify!',
+      message: `Currently playing ${data.item.name} by ${data.item.artists[0].name} on Spotify`,
       data: {
         artist: data.item.artists[0].name,
         track: data.item.name,
