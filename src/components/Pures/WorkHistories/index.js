9ebf4c139a068b1ad9848a506c9659ae222f8af5
Dimas Saputra <dimas.maulanads@gmail.com>
2022-08-11 15:25:09 +0700

    add work histories time

--- src/components/Pures/WorkHistories/index.js
+++ src/components/Pures/WorkHistories/index.js
@@ -8,6 +8,25 @@ const getFormattedDate = (dateString) => {
   return `${splitDate[1]} ${splitDate[3]}`;
 };
 
+const calculateWorkHistoriesTime = (startDate, endDate) => {
+  const startDateTime = new Date(startDate);
+  const endDateTime = new Date(endDate || new Date());
+  const timeDiff = Math.abs(endDateTime.getTime() - startDateTime.getTime());
+  const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+
+  // convert to x years y mounth format
+  const years = Math.floor(diffDays / 365);
+  const months = Math.floor((diffDays % 365) / 30);
+  const yearsText = years > 0 ? `${years} year${years > 1 ? 's' : ''}` : '';
+  const monthsText = months > 0 ? `${months} month${months > 1 ? 's' : ''}` : '';
+
+  if (yearsText && monthsText) {
+    return `${yearsText}, ${monthsText}`;
+  }
+
+  return yearsText || monthsText;
+};
+
 function WorkHistories() {
   return (
     <div className="work-histories">
@@ -28,6 +47,9 @@ function WorkHistories() {
                   -
                   {' '}
                   {endDate ? getFormattedDate(endDate) : 'Present'}
+                  <span className="work-histories__item__info__date_range">
+                    {calculateWorkHistoriesTime(startDate, endDate)}
+                  </span>
                 </p>
               </div>
             </div>
