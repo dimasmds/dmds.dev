36ffd1ed98d9de7a65100b15a8277ca8fed9dd01
Dimas Saputra <dimas.maulanads@gmail.com>
2022-08-11 16:36:27 +0700

    add colorful for tags

--- src/components/Pures/Tag/index.js
+++ src/components/Pures/Tag/index.js
@@ -3,12 +3,34 @@ import PropTypes from 'prop-types';
 
 import './style.scss';
 
+const materialColorfulPallettes = [
+  '#F44336',
+  '#E91E63',
+  '#9C27B0',
+  '#673AB7',
+  '#3F51B5',
+  '#2196F3',
+  '#03A9F4',
+  '#00BCD4',
+  '#009688',
+  '#4CAF50',
+  '#8BC34A',
+];
+
+function getRandomColor() {
+  const index = Math.floor(Math.random() * materialColorfulPallettes.length);
+  return materialColorfulPallettes[index];
+}
+
 function Tag(props) {
   const { name, icon: Icon } = props;
   return (
     <div className="tag">
       <div>
-        <Icon />
+        <Icon style={{
+          color: getRandomColor(),
+        }}
+        />
         <p className="tag__name">{name}</p>
       </div>
 
