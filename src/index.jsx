5d5faaa906382db280a4875cd9c839089df5b644
Dimas Saputra <dimas.maulanads@gmail.com>
2024-01-31 09:30:55 +0700

    change rendering approach

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './components/App';
 
 // global styles
 import './styles/global.scss';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
 );
