4edae2b6ffe7729792ad0c7db31e252c2ba389d8
Dimas Saputra <dimas.maulanads@gmail.com>
2024-01-31 11:46:46 +0700

    remove usage of config

--- netlify/commons/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  spotify: {
-    clientId: process.env.SPOTIFY_CLIENT_ID,
-    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-    refreshToken: process.env.SPOTIFY_REFRESH_TOKEN,
-  },
-};
-
-export default config;
